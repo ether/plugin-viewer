1e55302dcbd2c1b4e9e23fa5fd06c579b70e3c3f
Stefan Müller <mu.stefan@googlemail.com>
2020-04-19 16:23:34 +0200

    Fix wrong import for Plugin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './Plugin';
+import Plugin from './Plugin';
 
 class App extends React.Component {
   constructor(props) {
